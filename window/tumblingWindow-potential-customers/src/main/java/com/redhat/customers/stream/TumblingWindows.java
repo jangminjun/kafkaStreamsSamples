5269d1bda980f5fbd05240b9b0868be5945e0bbc
jangminjun <38595676+jangminjun@users.noreply.github.com>
2024-02-22 20:10:18 +0900

    del static void

--- window/tumblingWindow-potential-customers/src/main/java/com/redhat/customers/stream/TumblingWindows.java
+++ window/tumblingWindow-potential-customers/src/main/java/com/redhat/customers/stream/TumblingWindows.java
@@ -11,6 +11,13 @@ import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Printed;
 import org.apache.kafka.streams.kstream.TimeWindows;
 import org.jboss.logging.Logger;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.common.config.SslConfigs;
+import org.apache.kafka.streams.StreamsConfig;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import java.util.Properties;
+import java.util.Random;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
@@ -18,17 +25,33 @@ import javax.enterprise.inject.Produces;
 import java.time.Duration;
 
 @ApplicationScoped
-public class TumblingWindows extends StreamProcessor {
+public class TumblingWindows  {
     private static final Logger LOGGER = Logger.getLogger(TumblingWindows.class);
 
     // Reading topic
     static final String POTENTIAL_CUSTOMERS_TOPIC = "potential-customers-detected";
     static final int WINDOW_SIZE = 10;
+    private final Random random = new Random();
+    protected Properties generateStreamConfig() {
+        Properties props = new Properties();
+
+        props.put(
+                StreamsConfig.APPLICATION_ID_CONFIG,
+                this.getClass().getSimpleName() + random.nextInt()
+        );
+
+        props.put(
+                StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "my-cluster-kafka-bootstrap.efnalf-kafka-cluster.svc:9092"
+        );
+
+        return props;
+    }
 
     @Produces
     private KafkaStreams streams;
 
-    static void onStart(@Observes StartupEvent startupEvent) {
+    void onStart(@Observes StartupEvent startupEvent) {
         StreamsBuilder builder = new StreamsBuilder();
 
         ObjectMapperSerde<PotentialCustomersWereDetected> customersEventSerde
@@ -54,7 +77,7 @@ public class TumblingWindows extends StreamProcessor {
         streams.start();
     }
 
-    static void onStop(@Observes ShutdownEvent shutdownEvent) {
+    void onStop(@Observes ShutdownEvent shutdownEvent) {
         streams.close();
     }
 }
