a4cf41e342118fdb1e97ccb811fe8b020a1b7782
jangminjun <38595676+jangminjun@users.noreply.github.com>
2024-02-23 01:13:12 +0900

    conf

---
+++ window/tumblingWindow-potential-customers/src/main/java/com/redhat/customers/stream/StreamProcessor.java
@@ -0,0 +1,35 @@
+package com.redhat.customers.stream;
+
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.common.config.SslConfigs;
+import org.apache.kafka.streams.StreamsConfig;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import java.util.Properties;
+import java.util.Random;
+
+public abstract class StreamProcessor {
+    private final Random random = new Random();
+
+    @ConfigProperty(name = "quarkus.kafka-streams.bootstrap-servers")
+    String bootstrapServers;
+
+
+    protected Properties generateStreamConfig() {
+        Properties props = new Properties();
+
+
+
+        props.put(
+                StreamsConfig.APPLICATION_ID_CONFIG,
+                this.getClass().getSimpleName() + random.nextInt()
+        );
+
+        props.put(
+                StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapServers
+        );
+
+        return props;
+    }
+}
