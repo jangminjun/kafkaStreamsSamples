1b2f9fed43a381fe49b450b5ad0c51db8a397db3
jangminjun <38595676+jangminjun@users.noreply.github.com>
2024-02-22 14:33:56 +0900

    11000

--- window/producer-TimeWindowData/src/main/java/com/redhat/window/producer/ProducerApp.java
+++ window/producer-TimeWindowData/src/main/java/com/redhat/window/producer/ProducerApp.java
@@ -33,7 +33,7 @@ public class ProducerApp {
 
                         LOG.infov("Event sent : {0}, {1} - (late)", num, currentTime);
 
-                        currentTime = currentTime - 1100;
+                        currentTime = currentTime - 11000;
                         num=0;
                         try{
                             Thread.sleep(5000);
